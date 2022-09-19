b2c8d6f87988c3ce65a70982628efd0dc0581db6
Remix Workspace <>
2022-09-19 19:28:53 +0900

    update

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -12,4 +12,16 @@ contract StorageFactory {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint _simpleStorageNumber) public {
+        // Address
+        // ABI
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStorage.retrieve();
+    }
 }
