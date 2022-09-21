2ad284939b298cc37889c3d752b1cfd8affbb908
Remix Workspace <>
2022-09-21 13:06:26 +0900

    UPDATE

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,6 +2,35 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+interface AggregatorV3Interface {
+
+  function decimals() external view returns (uint8);
+  function description() external view returns (string memory);
+  function version() external view returns (uint256);
+  function getRoundData(uint80 _roundId)
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+}
+
 contract FundMe {
 
     mapping(address => uint256) public addressToAmountFunded;
@@ -10,4 +39,10 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
         // what the ETH -> USD conversion rate 
     }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        return priceFeed.version();
+    }
+
 }
