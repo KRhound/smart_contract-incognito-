84a33b070ddc825b289e33113e92c98cbe87aacb
Remix Workspace <>
2022-09-23 12:20:48 +0900

    UPDATE

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -33,16 +33,30 @@ interface AggregatorV3Interface {
 
 contract FundMe {
 
-    mapping(address => uint256) public addressToAmountFunded;
+  mapping(address => uint256) public addressToAmountFunded;
 
-    function fund() public payable {
-        addressToAmountFunded[msg.sender] += msg.value;
-        // what the ETH -> USD conversion rate 
-    }
+  function fund() public payable {
+    // $50
+    addressToAmountFunded[msg.sender] += msg.value;
+    // what the ETH -> USD conversion rate 
+  }
 
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
-        return priceFeed.version();
-    }
+  function getVersion() public view returns (uint256) {
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    return priceFeed.version();
+  }
+
+  function getPrice() public view returns(uint256){
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    (,int256 answer,,,) = priceFeed.latestRoundData();
+    return uint256(answer = 10000000000);
+  }
+
+  // 10000000000
+  function getConversionRate(uint256 ethAmount) public view returns (uint256){
+    uint256 ethPrice = getPrice();
+    uint256 ethAmountInUsd = (ethPrice * ethAmount);
+    return ethAmountInUsd;
+  }
 
 }
---
+++ contracts/Overflow.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Overflow{
+
+
+    function overflow() public view returns(uint8){
+        uint8 big = 255 + uint8(100);
+        return big;
+    }
+}
